b0b4e8e9bc2aae9f6e1275d370a6687e2e49c839
Leandro <leandrolupano@hotmail.com>
2023-03-13 18:27:44 +0100

    🏗️ Add repo interfaces

--- src/repositories/repo.interface.ts
+++ src/repositories/repo.interface.ts
@@ -1,3 +1,8 @@
 export interface Repo<T> {
-  query(): Promise<T[]>;
+  read(): Promise<T[]>;
+  readId(id: string): Promise<T>;
+  create(info: Partial<T>): Promise<T>;
+  update(info: Partial<T>): Promise<T>;
+  erase(id: string): Promise<void>;
+  search(query: { key: string; value: unknown }): Promise<T[]>;
 }
