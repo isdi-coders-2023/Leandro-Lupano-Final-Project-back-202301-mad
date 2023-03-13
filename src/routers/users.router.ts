2f66ddf98c4b6f518d78db0921ad2150e6b9aec9
Leandro <leandrolupano@hotmail.com>
2023-03-13 21:19:03 +0100

    🏗️ Add login and register routes

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,8 +1,14 @@
 import { Router as router } from 'express';
+import { UsersController } from '../controllers/users.controller.js';
+import { UsersMongoRepo } from '../repositories/users.mongo.repo.js';
 import createDebug from 'debug';
 
 const debug = createDebug('GW:router');
-
-debug('Users Router');
+debug('users-router');
 
 export const usersRouter = router();
+const usersRepo = new UsersMongoRepo();
+const controller = new UsersController(usersRepo);
+
+usersRouter.post('/register', controller.register.bind(controller));
+usersRouter.post('/login', controller.login.bind(controller));
